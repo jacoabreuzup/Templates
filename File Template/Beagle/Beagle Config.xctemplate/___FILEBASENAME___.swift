3eb938badac520249184672731771530142a3835
jacoabreuzup <joao.abreu@zup.com.br>
2021-04-14 21:48:53 -0300

    BeagleConfig and gitignore

--- File Template/Beagle/Beagle Config.xctemplate/___FILEBASENAME___.swift
+++ File Template/Beagle/Beagle Config.xctemplate/___FILEBASENAME___.swift
@@ -3,13 +3,25 @@
 import Foundation
 import Beagle
 
-class ___FILEBASENAME___: AnalyticsProvider {
-    func getConfig() -> AnalyticsConfig {
-    
+class ___FILEBASENAME___ {
+
+    private init() {  }
+
+    static func start() {
+        
+        let dependencies = BeagleDependencies()
+        
+        /**
+         Here you can override Beagle dependencies as you want to, with your own implementations of each one.
+         We strongly recommend that you register at least a network layer since Beagle needs to decode JSON coming from your BFF
+         
+         Check our documentation here: https://docs.usebeagle.io
+        */
+        
+        /// Network client definition example
+        // dependencies.networkClient = CustomNetworkClient()
+
+        Beagle.dependencies = dependencies
     }
-    
-    func createRecord(_ record: AnalyticsRecord) {
-    
-    }
-    
+
 }
